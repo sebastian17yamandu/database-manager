bb23eecb6a3f2bdbcb9093687b0ec316b182259b
Sebastião Medeiros <sebastian.tecinf@gmail.com>
2022-04-15 10:20:17 -0500

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+  namespace App\Config;
+
+  //Inserir os dados referente a conexão do banco de dados
+
+  define('HOSTNAME', 'localhost');
+  define('DATABASE', 'nome_banco');
+  define('USERDATABASE', 'user_banco');
+  define('PPWDDATABASE', 'senha_banco');
+  define('PORTDATABASE', '3306'); //Geralmente usado no mysql
